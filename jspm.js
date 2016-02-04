684207efa19d491018b72b42af9da29d8dd1489b
Rafał Krzewski <rafal.krzewski@caltha.pl>
2016-02-04 20:54:39 +0100

    added ng-router dependency

--- jspm.js
+++ jspm.js
@@ -9,10 +9,14 @@ System.config({
 
   map: {
     "angular": "npm:angular@1.4.8",
+    "angular-route": "github:angular/bower-angular-route@1.4.9",
     "bootstrap-sass": "github:twbs/bootstrap-sass@3.3.6",
     "html": "github:jamespamplin/plugin-ng-template@0.1.1",
     "ng-facebook": "github:GoDisco/ngFacebook@0.1.6",
     "roboto-fontface": "github:choffmeister/roboto-fontface-bower@0.4.3",
+    "github:angular/bower-angular-route@1.4.9": {
+      "angular": "github:angular/bower-angular@1.4.9"
+    },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
